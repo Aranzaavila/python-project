73cca7ef8ec833e2d733b7ce35b9b29ea96701a1
aranza.avila <ariavirod@gmail.com>
2024-12-10 11:28:54 +0100

    process

--- project.py
+++ project.py
@@ -10,32 +10,44 @@ def choose_warm(warm_list):
 def choose_cold(cold_list):
  return random.choice(cold_list)
 
-activities = {
- "Mexico": ["visit cancun", "go to cdmx"],
- "Venezuela": ["vist ....", "go to..."],
- "Canada": ["go to quebec", "ski"],
- "Russia": ["", ]
+def activities_countries(country):
+ activities = {
+  "Mexico": ["visit cancun", "go to cdmx"],
+  "Venezuela": ["vist ....", "go to..."],
+  "Canada": ["go to quebec", "ski"],
+  "Russia": ["drink", "vodka"]
 }
+ return activities.get(country)
 
 
 while True:
  print("Choose the type of weather you would like to have in your vacation")
  print("\n 1 - warm \n 2 - cold")
  choice = input("Type warm or cold: ").lower()
+
  if choice == "warm":
   warm = choose_warm(warm)
   print(f"You should go to: {warm}")
-  break
-
   
+  user_answer = input("Write the country that was choosen to know activities or type exit to end it: ").strip()
+
+  if user_answer.lower() == "exit":
+   print("Bye bye!")
+   break
+  
+  else:
+   activities = activities_countries(user_answer)
+   print(f"\n Popular activities in {user_answer}:")
+   for i, activities in enumerate(activities, start=1):
+    print(f"{i}. {activities}")
+
  
  
  elif choice == "cold":
   cold = choose_cold(cold)
-  print(f"You should go to: {cold}")
-  user_answer = input(f"Do you want to know some activities from {cold}?")
-  if user_answer == "yes":
+  result = print(f"You should go to: {cold}")
 
+  
 
   
   
