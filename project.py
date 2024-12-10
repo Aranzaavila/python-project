25a14724a6c1d7abf24ca92bf202d219396aa74c
aranza.avila <ariavirod@gmail.com>
2024-12-10 18:07:34 +0100

    process

--- project.py
+++ project.py
@@ -4,59 +4,71 @@ warm = ["Mexico", "Venezuela"]
 
 cold = ["Canada", "Russia"]
 
-def choose_warm(warm_list):
- return random.choice(warm_list)
+def choose_warm(warm):
+    print(f"You should go to: {random.choice(warm)}")
 
-def choose_cold(cold_list):
- return random.choice(cold_list)
+def choose_cold(cold):
+    return random.choice(cold)
 
 def activities_countries(country):
- activities = {
-  "Mexico": ["visit cancun", "go to cdmx"],
-  "Venezuela": ["vist ....", "go to..."],
-  "Canada": ["go to quebec", "ski"],
-  "Russia": ["drink", "vodka"]
+    activities = {
+    "Mexico": ["visit ", "go to "],
+    "Venezuela": ["vist ....", "go to..."],
+    "Canada": ["go to", ""],
+    "Russia": ["", ""]
 }
- return activities.get(country)
+    return activities.get(country)
+    
+
+def main():
+    while True:
+        print("")
+        print("Choose the type of weather you would like to have in your vacation")
+        print("1. warm")
+        print("2. cold")
+        print("3. Exit")
+        print("")
+        try:
+            choice = int(input("Choose an option: "))
+
+            if choice == 1:
+                choose_warm(warm)
+                if warm:
+                    user_answer = input("Do you wanna know the activities about this country? (yes/no): ").strip().lower()
+                    if user_answer == "yes":
+                        country_activities = input("Write the name of the country, please: ").capitalize().strip()
+                        
+                        activities = activities_countries(country_activities)
+                        print(f"\n Popular activities in {country_activities}:")
+                        for i, activities in enumerate(activities, start=1):
+                            print(f"{i}. {activities}")
+                        break
+                    
+                    elif user_answer == "no":
+                        print("Bye bye!")
+                        break
+                    else:
+                        print("Input not valid, please enter (yes/no).")
+                        print("")
+                else:
+                    continue
 
 
-while True:
- print("Choose the type of weather you would like to have in your vacation")
- print("\n 1 - warm \n 2 - cold")
- choice = input("Type warm or cold: ").lower()
+            elif choice == 2:
+                choose_cold(cold)
+                result = print(f"You should go to: {cold}")
 
- if choice == "warm":
-  warm = choose_warm(warm)
-  print(f"You should go to: {warm}")
-  
-  user_answer = input("Write the country that was choosen to know activities or type exit to end it: ").strip()
+            elif choice == 3:
+                print("Thanks for all!")
+                print("Hope to see you soon.")
+                print("")
+                break
 
-  if user_answer.lower() == "exit":
-   print("Bye bye!")
-   break
-  
-  else:
-   activities = activities_countries(user_answer)
-   print(f"\n Popular activities in {user_answer}:")
-   for i, activities in enumerate(activities, start=1):
-    print(f"{i}. {activities}")
+            else:
+                print("Type a valid input")
+                print("")
+        except ValueError:
+           print("Input not valid, try again.")
+           print("")
 
- 
- 
- elif choice == "cold":
-  cold = choose_cold(cold)
-  result = print(f"You should go to: {cold}")
-
-  
-
-  
-  
-
- else:
-  print("Type a valid input")
-
-
-  
-
-
-   
+main()
