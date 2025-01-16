a8ac9d7cad0f672a05db1427221af25d53837608
aranza.avila <ariavirod@gmail.com>
2025-01-17 00:07:09 +0100

    GUI

--- GUI.py
+++ GUI.py
@@ -1,11 +1,12 @@
-from tkinter import Tk
+import tkinter as tk
+from tkinter import messagebox
 import random
+
 
 warm = ["Mexico", "Venezuela", "Spain", "Dominican Republic", "Portugal"]
 cold = ["Canada", "Russia", "Finland", "Norway", "Iceland"]
 
-def activities_countries(country):
-    activities = {
+activities = {
     "Mexico": ["Xcaret", "Chichen Itza ", "The Chapultepec Castle", "Great Pyramid of Cholula", "Tulum Ruins", "Monte Alban"],
     "Venezuela": ["Thundering Angel Falls", "Tucacas", "Isla de Margarita (Margarita Island)", "Morrocoy National Park)", "Canaima National Park and the Gran Sabana", "Roraima"],
     "Spain": ["The Sagrada Familia", "Alhambra Palace Granada","The Royal Palace Madrid","Seville Cathedral","Santiago de Compostela Cathedral","Costa de la Luz Beaches" ],
@@ -17,18 +18,29 @@ def activities_countries(country):
     "Canada": ["Niagara Fall", "Banff National Park & the Rocky Mountains", "Stanley Park", "Notre-Dame Basilica", "Toronto's CN Tower", "St. John's Signal Hill National Historic Site"],
     "Russia": ["Red Square",  "Kremlin", "Novodevichy Convent", "Christ the Saviour Cathedral", "Kolomenskoe Estate", "Arbat street and Victory Park"]
 }
-    
-def weather_place(weather_type):
-    if weather_type== "warm":
-        country= random.choice(warm)
-    elif weather_type== "cold":
-       country= random.choice(cold)
 
 
 
-root= Tk()
-root.title("Random destination game")
-root.geometry("400x500")
 
+
+#GUI window
+window= tk.Tk()
+window.title("Random destination game")
+window.geometry("400x500")
+window.config(background= "#a6ffe4")
+
+#labels
+photo= tk.PhotoImage(file="welcome.png")
+label_intro= tk.Label(window,
+                  text= "Welcome to the game!",
+                  font=("Arial", 30), 
+                  fg="#0a0a57",
+                  image= photo,
+                  compound= "bottom")
+label_intro.pack()
+
+label_desicion= tk.Label(window, text="Choose the type of weather for yout vacation:",
+                      font=("Arial, 12"))
+label_desicion.pack()
        
-root.mainloop()
+window.mainloop()
