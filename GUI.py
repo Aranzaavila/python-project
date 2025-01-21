a042faafb64c5f7af7b3e86788a70f1a14038adc
aranza.avila <ariavirod@gmail.com>
2025-01-21 22:10:48 +0100

    .

--- GUI.py
+++ GUI.py
@@ -37,7 +37,8 @@ def show_activities():
  else:
         messagebox.showwarning("No activities", f"No activities found for {selected_country }")
 
-
+def changeBG():
+    window.config(background= "red")
 def exit_game():
  window.destroy()
     
@@ -49,10 +50,11 @@ window.title("Random destination game")
 window.geometry("500x500")
 window.config(background= "#f7f0dc")
 
+bg= PhotoImage(file="bg.png")
+
+
 #creates label widgets
-
-
-label_intro= tk.Label(window, text= "Welcome to the game!", font=("Impact", 40, "bold"), fg="#0a6121", bg= "#91eda3")
+label_intro= tk.Label(window, text= "Welcome to the game!", font=("Impact", 40, "bold"), fg="#0a6121", bg= "#91eda3", width=20, height=2)
 label_intro.pack()
 
 label_desicion= tk.Label(window, text="Choose the type of weather for your vacation:", font=("Arial", 12, "bold"), bg="#91eda3", fg="#0a6121" )
@@ -61,9 +63,10 @@ label_desicion.pack(pady=10, padx=10)
 frame_buttons= tk.Frame(window, bg= "#f7f0dc")
 frame_buttons.pack(pady=22, padx=10)
 
-button_warm= tk.Button(frame_buttons, text= "Warm",bg= "#ed7272", fg="black", font=("Arial", 12, "bold"), command=lambda: weather_place("warm")) 
+button_warm= tk.Button(frame_buttons, text= "🌞Warm🌞",bg= "#ed7272", fg="black", font=("Arial", 12, "bold"), command= lambda: weather_place("warm")) 
+button_warm(command=changeBG)
 button_warm.grid(row=0, column=0, padx=10, pady=10)
-button_cold= tk.Button(frame_buttons, text="Cold", fg= "white", bg="#8092ed", font=("Arial", 12, "bold"), command= lambda: weather_place("cold") )
+button_cold= tk.Button(frame_buttons, text="❄️Cold❄️", fg= "white", bg="#8092ed", font=("Arial", 12, "bold"), command= lambda: weather_place("cold") )
 button_cold.grid(row=0, column=1, padx=10, pady=10)
 
 label_result= tk.Label(window, text= "Your next trip will be in:", fg="#0a6121", bg="#91eda3", font=("Arial", 12, "bold"), pady=10)
